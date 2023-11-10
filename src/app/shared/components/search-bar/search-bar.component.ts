fe73d9e526e7ce99657135fb12cd6aad5f0b1e4c
Alex Jeremías Loza <alexjeremiasloza@gmail.com>
2023-11-10 00:56:38 -0300

    Add the search-bar component
    - Add the search bar with their functionalities

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { DrinkService } from 'src/app/get-drunk/services/drink.service';
 
 @Component({
   selector: 'shared-search-bar',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+  public newTag: string = '';
 
+  constructor(private drinkService: DrinkService, private router: Router) { }
+
+  searchTag() {
+    this.newTag = this.tagInput.nativeElement.value;
+
+    if(this.newTag === '') return;
+
+    this.router.navigate(['/search', this.newTag]);
+
+    this.tagInput.nativeElement.value = '';
+  }
 }
