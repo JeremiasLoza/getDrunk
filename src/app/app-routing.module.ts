3ce74ad35373141e006482fd0ecce687292eba8d
Alex Jeremías Loza <alexjeremiasloza@gmail.com>
2023-11-12 20:39:14 -0300

    Add 404 not found page
    - Add the 404 not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,17 @@ import { DrinkPageComponent } from './get-drunk/components/drink-page/drink-page
 import { DrinkListComponent } from './get-drunk/components/drink-list/drink-list.component';
 import { SearchDrinkComponent } from './get-drunk/components/search-drink/search-drink.component';
 import { FavoritesComponent } from './auth/components/favorites/favorites.component';
+import { NotFoundComponent } from './get-drunk/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'drink/:id', component: DrinkPageComponent },
   { path: 'favorites', component: FavoritesComponent },
-  { path: 'search/:name', component: SearchDrinkComponent }, // TODO: Armar el componente con la busqueda
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'search/:name', component: SearchDrinkComponent },
+  { path: 'category/:name', component: SearchDrinkComponent }, // TODO: Armar el componente con la busqueda
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
