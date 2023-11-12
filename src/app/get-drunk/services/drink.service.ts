132b03e58b6ba0fb5272e530a5c2ce694372f5ce
Alex Jeremías Loza <alexjeremiasloza@gmail.com>
2023-11-12 20:41:31 -0300

    Add new functions and add the API Key
    - Add the popular drinks function.
    - Add the new API Key.

--- src/app/get-drunk/services/drink.service.ts
+++ src/app/get-drunk/services/drink.service.ts
@@ -11,10 +11,11 @@ export class DrinkService {
   public drinkList: Drink[] = [];
   public drink: Drink[] = [];
   public categoryList: Category[] = [];
+  public popularDrinks: Drink[] = [];
 
   constructor(private http: HttpClient) { }
 
-  private serviceUrl: string = 'https://www.thecocktaildb.com/api/json/v1/1/';
+  private serviceUrl: string = 'https://www.thecocktaildb.com/api/json/v2/9973533/';
 
   searchDrink(tag: string): void {
     if (tag.length === 0) return;
@@ -40,13 +41,21 @@ export class DrinkService {
       });
   }
 
-  getCategories(){
+  getCategories(): void {
     const params = new HttpParams().set('c', 'list');
 
     this.http
       .get<CategoryResponse>(`${this.serviceUrl}list.php`, { params })
       .subscribe((resp) => {
-        this.categoryList = resp.categories;
+        this.categoryList = resp.drinks;
+      });
+  }
+
+  getPopularDrinks(): void {
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}popular.php`)
+      .subscribe((resp) => {
+        this.popularDrinks = resp.drinks;
       });
   }
 }
