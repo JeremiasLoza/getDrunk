ffa4ccecffd15acc6c5d24c6f44ce742837f92cc
Alex Jeremías Loza <alexjeremiasloza@gmail.com>
2023-11-12 20:42:45 -0300

    Add modules

--- src/app/get-drunk/get-drunk.module.ts
+++ src/app/get-drunk/get-drunk.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { DrinkListComponent } from './components/drink-list/drink-list.component';
 import { DrinkCardComponent } from './components/drink-card/drink-card.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { DrinkPageComponent } from './components/drink-page/drink-page.component';
 import { SearchDrinkComponent } from './components/search-drink/search-drink.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { IngredientsListComponent } from './components/ingredients-list/ingredients-list.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +21,13 @@ import { SearchDrinkComponent } from './components/search-drink/search-drink.com
     CarrouselComponent,
     DrinkPageComponent,
     SearchDrinkComponent,
+    NotFoundComponent,
+    IngredientsListComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ],
   exports: [HomePageComponent],
 })
