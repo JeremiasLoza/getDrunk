13de87f04fe28b73cedc2bdcb5d07d739d675243
sofiabelber <sofibelber@gmail.com>
2023-11-15 00:35:44 -0300

    Register page

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router'
+import { AuthLoginService } from 'src/app/get-drunk/services/auth.login.service';
 
 @Component({
   selector: 'auth-register',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  userForm = this.formBuilder.group({
+    name: ['',[Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+    lastName: ['',[Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+    email: ['',[Validators.required, Validators.email]],
+    password: ['',[Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder, private loginService: AuthLoginService) { }
+
+  ngOnInit() {
+    
+  }
+
+  saveUser(){
+    console.log(this.userForm.value);
+    this.loginService.saveUserData(this.userForm.value);
+  }
 
 }
