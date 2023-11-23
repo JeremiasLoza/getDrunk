b036146089cb2934d1d15d0e8f9c99bacbf1d804
Alex Jeremías Loza <alexjeremiasloza@gmail.com>
2023-11-23 10:33:57 -0300

    Fix favorites, and about us

---
+++ src/app/get-drunk/components/category/category.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'get-drunk-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+})
+export class CategoryComponent { }
